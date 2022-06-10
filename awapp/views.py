3b5ef4dde01738e2ccf55d4ae548e4be65bb1512
lcmongwe <lcmongwe@gmail.com>
2022-06-10 13:52:12 +0300

    login and register forms created

--- awapp/views.py
+++ awapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from .models import *
+import datetime as dt
+from .forms import *
+# from django.core.mail import send_mail
+# from django.conf import settings
 
-# Create your views here.
+
+def home(request):
+    
+    return render(request, 'home.html', {})
