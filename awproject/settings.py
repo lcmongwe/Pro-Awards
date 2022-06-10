98261d4d09b0d9c5d0c057da1faacc6ac85c1620
lcmongwe <lcmongwe@gmail.com>
2022-06-10 10:06:01 +0300

    installed psycopg2 database configurations set

--- awproject/settings.py
+++ awproject/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'awproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awards',
+        'USER': 'lucy',
+    'PASSWORD':'4444',
     }
 }
 
