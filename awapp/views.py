459ec09e81732bfc89d5fb3bc981fab009c38829
lcmongwe <lcmongwe@gmail.com>
2022-06-12 19:53:54 +0300

    api vew created for posts

--- awapp/views.py
+++ awapp/views.py
@@ -101,3 +101,46 @@ def search(request):
     else:
         return render(request, 'searched.html',{})
 
+
+
+
+
+
+#  API VIEWS
+
+@api_view(['GET','POST'])
+def post_list(request):
+    if request.method == 'GET':
+        posts=Post.objects.all()
+        serializer = Postserializer(posts,many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+       serializer = Postserializer(data=request.data)
+       if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+@api_view(['GET','PUT','DELETE'])
+def drink_detail(request,id):
+
+    try:
+       post= Post.objects.get(pk=id)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+   
+    if request.method == 'GET':
+       serializer=Postserializer(post)
+       return Response(serializer.data)
+
+    elif request.method == 'PUT':
+       serializer=Postserializer(post, data=request.data)
+       if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.dat)
+       return Response(serializer.errors,status=status.HTTP_404_BAD_REQUEST)
+        
+
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
